3b69dff7d3d83dd999d49cae1885e906c5fb657b
Calvin May <calvinkylemay.dev@gmail.com>
2025-04-18 10:36:30 -0400

    Configured Stack Navigation with a single screen component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// React & React-Native Imports
+//...
 
+// React Navigation
+import { NavigationContainer } from '@react-navigation/native';
+
+// Navigators
+import { RootNavigator } from './Navigation';
+
+// App
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style='auto' />
-    </View>
+    <NavigationContainer>
+      <RootNavigator />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/SearchScreen.tsx
@@ -0,0 +1,17 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+const SearchScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>SearchScreen</Text>
+    </View>
+  );
+};
+
+export default SearchScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
