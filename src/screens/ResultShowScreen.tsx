c76fdc4d3d19c32f1f419004b5730b288188263f
Calvin May <calvinkylemay.dev@gmail.com>
2025-04-22 07:36:27 -0400

    Restuarant Photos are now displayed on the ResultShowScreen component

--- src/screens/ResultShowScreen.tsx
+++ src/screens/ResultShowScreen.tsx
@@ -1,14 +1,39 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Image } from 'react-native';
+import { useEffect, useState } from 'react';
 import type { RootStackScreenProps } from '@/app/navigation';
+import yelp from '@/api/yelp';
+import { YelpResult } from '@/types/types';
+
 const ResultShowScreen = ({
   route,
   navigation,
 }: RootStackScreenProps<'ResultShowScreen'>) => {
   const resultID = route.params.resultID;
+  const [result, setResult] = useState<YelpResult | null>(null);
 
+  const getResult = async (id: string) => {
+    const response = await yelp.get(`/${id}`);
+    console.log(response.data);
+    setResult(response.data);
+  };
+
+  useEffect(() => {
+    getResult(resultID);
+  }, []);
+
+  if (!result) {
+    return null;
+  }
   return (
     <View style={styles.container}>
-      <Text>ResultShowScreen: {resultID}</Text>
+      <Text>{result.name}</Text>
+      <FlatList
+        data={result.photos}
+        keyExtractor={(photo) => photo}
+        renderItem={({ item }) => {
+          return <Image style={styles.image} source={{ uri: item }} />;
+        }}
+      />
     </View>
   );
 };
@@ -18,5 +43,10 @@ export default ResultShowScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  image: {
+    height: 200,
+    width: 300,
+    margin: 12,
   },
 });
--- src/types/types.ts
+++ src/types/types.ts
@@ -18,6 +18,7 @@ type YelpResult = {
     zip_code: string;
   };
   name: string;
+  photos: [];
   phone: string;
   price: string;
   rating: number;
@@ -25,5 +26,55 @@ type YelpResult = {
   transactions: [];
   url: string;
 };
+
+// Sample Data Structure returned by th API
+// const data = {
+//   alias: 'what-a-bagel-ajax-2',
+//   categories: [
+//     { alias: 'bagels', title: 'Bagels' },
+//     { alias: 'breakfast_brunch', title: 'Breakfast & Brunch' },
+//     { alias: 'bakeries', title: 'Bakeries' },
+//   ],
+//   coordinates: { latitude: 43.8787194011473, longitude: -79.0247792264587 },
+//   display_phone: '+1 905-426-4426',
+//   hours: [{ hours_type: 'REGULAR', is_open_now: true, open: [Array] }],
+//   id: 'Y_TXOBZxfUjPYoQuEF2aAA',
+//   image_url:
+//     'https://s3-media1.fl.yelpcdn.com/bphoto/Bopig-862GNLts_TmZxYyQ/o.jpg',
+//   is_claimed: true,
+//   is_closed: false,
+//   location: {
+//     address1: '1 Rossland Road E',
+//     address2: 'Unit F1.4',
+//     address3: '',
+//     city: 'Ajax',
+//     country: 'CA',
+//     cross_streets: '',
+//     display_address: [
+//       '1 Rossland Road E',
+//       'Unit F1.4',
+//       'Ajax, ON L1Z 0T4',
+//       'Canada',
+//     ],
+//     state: 'ON',
+//     zip_code: 'L1Z 0T4',
+//   },
+//   messaging: {
+//     url: 'https://www.yelp.com/raq/Y_TXOBZxfUjPYoQuEF2aAA?adjust_creative=W1dNi6Ziy5O0z-dk2LOQow&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_lookup&utm_source=W1dNi6Ziy5O0z-dk2LOQow#popup%3Araq',
+//     use_case_text: 'Message the Business',
+//   },
+//   name: 'What A Bagel',
+//   phone: '+19054264426',
+//   photos: [
+//     'https://s3-media1.fl.yelpcdn.com/bphoto/Bopig-862GNLts_TmZxYyQ/o.jpg',
+//     'https://s3-media1.fl.yelpcdn.com/bphoto/hBicJPwD9-yuZbo6u4znVg/o.jpg',
+//     'https://s3-media2.fl.yelpcdn.com/bphoto/NUU_9aIPudsApsgZbOt-IQ/o.jpg',
+//   ],
+//   price: '$',
+//   rating: 4.5,
+//   review_count: 38,
+//   transactions: [],
+//   url: 'https://www.yelp.com/biz/what-a-bagel-ajax-2?adjust_creative=W1dNi6Ziy5O0z-dk2LOQow&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_lookup&utm_source=W1dNi6Ziy5O0z-dk2LOQow',
+// };
 
 export { YelpResult };
